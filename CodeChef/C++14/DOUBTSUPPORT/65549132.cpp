a38f56f48d3bd60d7de4d5a90946e5be8f64744c
khaled-diab <55098152+khaled-diab@users.noreply.github.com>
2022-06-04 23:40:26 +0200

    Committing files

---
+++ CodeChef/C++14/DOUBTSUPPORT/65549132.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+#define endl "\n"
+
+using namespace std;
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+    cout << (n <= 1600 ? "YES":"NO");
+}
+
+
+
+
+
