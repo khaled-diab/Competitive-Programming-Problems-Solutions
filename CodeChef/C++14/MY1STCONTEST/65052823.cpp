43416e2248b8e9320034f1860522524aff15afba
khaled-diab <55098152+khaled-diab@users.noreply.github.com>
2022-06-04 23:40:34 +0200

    Committing files

---
+++ CodeChef/C++14/MY1STCONTEST/65052823.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+#define endl "\n"
+using namespace std;
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, a, b;
+    cin >> n >> a >> b;
+    cout << n - a << " " << n - a - b;
+}
