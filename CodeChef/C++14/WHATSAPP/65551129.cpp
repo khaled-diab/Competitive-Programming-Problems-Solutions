2b1f7494b0e1f178d7a8e7be7909105cf83d08b6
khaled-diab <55098152+khaled-diab@users.noreply.github.com>
2022-06-04 23:40:27 +0200

    Committing files

---
+++ CodeChef/C++14/WHATSAPP/65551129.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+#define endl "\n"
+
+using namespace std;
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int x, y, z;
+        cin >> x >> y >> z;
+        cout << (x * z) - (y * z) << endl;
+    }
+}
+
+
+
+
+
