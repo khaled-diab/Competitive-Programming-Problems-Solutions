cd3e5ba25317610a0d1950df59fad2a8374c321a
khaled-diab <55098152+khaled-diab@users.noreply.github.com>
2022-06-04 23:40:19 +0200

    Committing files

---
+++ CodeChef/C++14/CBSPEED/65808213.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+#define endl "\n"
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int x,y;
+    cin >> x >> y;
+    cout << (y > x?"YES":"NO");
+}
+
+
