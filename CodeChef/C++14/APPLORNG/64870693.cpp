6e1970eb0f6e5cf83db7080bad5c51934a2c0e09
khaled-diab <55098152+khaled-diab@users.noreply.github.com>
2022-06-04 23:40:39 +0200

    Committing files

---
+++ CodeChef/C++14/APPLORNG/64870693.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+#define endl "\n"
+using namespace std;
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int x, a, b;
+    cin >> x >> a >> b;
+    (a + b <= x)? cout <<"yes": cout <<"no";
+}
