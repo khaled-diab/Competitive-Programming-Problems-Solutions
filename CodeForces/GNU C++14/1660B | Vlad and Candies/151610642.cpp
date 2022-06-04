c6ea340adfda505b472b7b4c87c3b16657581ea3
khaled-diab <55098152+khaled-diab@users.noreply.github.com>
2022-06-04 23:42:46 +0200

    Committing files

---
+++ CodeForces/GNU C++14/1660B | Vlad and Candies/151610642.cpp
@@ -0,0 +1,29 @@
+#include "bits/stdc++.h"
+ #define endl "\n"
+using namespace std;
+  int main() {
+    ios_base::sync_with_stdio(0), cin.tie(0);
+     int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> candies(n);
+        for (int i = 0; i < n; ++i) {
+            cin >> candies[i];
+        }
+        if (n == 1) {
+            if (candies[0] == 1) {
+                cout << "YES" << endl;
+            } else {
+                cout << "NO" << endl;
+            }
+            continue;
+        }
+        sort(candies.begin(), candies.end());
+        if (candies[n-1] - candies[n - 2] <= 1)
+            cout << "YES" << endl;
+        else cout << "NO"<< endl;
+    }
+}
+   
