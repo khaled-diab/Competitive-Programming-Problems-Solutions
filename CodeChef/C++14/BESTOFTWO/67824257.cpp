64e8a199c1c144b2c3917c472aa0805bd99fc92c
khaled-diab <55098152+khaled-diab@users.noreply.github.com>
2022-06-30 15:17:37 +0200

    Committing files

---
+++ CodeChef/C++14/BESTOFTWO/67824257.cpp
@@ -0,0 +1,28 @@
+#include "bits/stdc++.h"
+
+#pragma GCC optimize("Ofast")
+
+using namespace std;
+
+#define endl            "\n"
+#define all(v)          v.begin(), v.end()
+#define debug(x)        cout << #x << " is " << x << endl
+#define pow(n, m)       (int)powl(n, m)
+#define clr(x, value)    memset(x, value, sizeof(x))
+
+using ll = long long;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int x, y;
+        cin >> x >> y;
+        cout << max(x, y) << endl;
+    }
+
+
+}
