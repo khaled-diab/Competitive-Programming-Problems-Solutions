aa98dafbb3739af434cb4da36f215ae97fbf2f65
khaled-diab <55098152+khaled-diab@users.noreply.github.com>
2022-11-24 00:03:43 +0200

    Committing files

---
+++ CodeForces/GNU C++17/1749A | Cowardly Rooks/177152057.cpp
@@ -0,0 +1,34 @@
+#include "bits/stdc++.h"
+ 
+#pragma GCC optimize("Ofast")
+ 
+using namespace std;
+ 
+#define endl            "\n"
+#define all(x)          x.begin(), x.end()
+#define debug(x)        cout << #x << " is " << x << endl
+ 
+typedef long long ll;
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+ 
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        for (int i = 0; i < m; ++i) {
+            int x, y;
+            cin >> x >> y;
+        }
+        if(m<n) cout << "YES" << endl;
+        else cout << "NO" << endl;
+    }
+ 
+ 
+}
+ 
+ 
