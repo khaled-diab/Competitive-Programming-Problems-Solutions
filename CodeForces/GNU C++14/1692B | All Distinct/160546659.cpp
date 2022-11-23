e7c4a8c218eca76aed0fc174006dfc0a32edd9ec
khaled-diab <55098152+khaled-diab@users.noreply.github.com>
2022-11-24 00:06:15 +0200

    Committing files

---
+++ CodeForces/GNU C++14/1692B | All Distinct/160546659.cpp
@@ -0,0 +1,37 @@
+#include "bits/stdc++.h"
+ 
+#pragma GCC optimize("Ofast")
+ 
+using namespace std;
+ 
+#define endl            "\n"
+#define all(v)          v.begin(), v.end()
+#define debug(x)        cout << #x << " is " << x << endl
+#define pow(n, m)       (int)powl(n, m)
+#define clr(x, value)    memset(x, value, sizeof(x))
+ 
+using ll = long long;
+ 
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; ++i) {
+            cin >> a[i];
+        }
+        sort(a, a + n);
+        ll c = 0;
+        for (int i = 0; i < n - 1; ++i) {
+            if (a[i] == a[i + 1]) c++;
+        }
+        cout << (c & 1 ? n - c - 1 : n - c) << endl;
+ 
+    }
+}
