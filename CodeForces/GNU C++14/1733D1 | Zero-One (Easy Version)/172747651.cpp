1a945417ee98bd454bd6deeaf985e53e760ea02c
khaled-diab <55098152+khaled-diab@users.noreply.github.com>
2022-11-24 00:04:56 +0200

    Committing files

---
+++ CodeForces/GNU C++14/1733D1 | Zero-One (Easy Version)/172747651.cpp
@@ -0,0 +1,43 @@
+#include "bits/stdc++.h"
+ 
+#pragma GCC optimize("Ofast")
+ 
+using namespace std;
+ 
+#define endl            "\n"
+#define all(v)          v.begin(), v.end()
+#define debug(x)        cout << #x << " is " << x << endl
+#define pow(n, m)       (int)powl(n, m)
+#define clr(x, value)    memset(x, value, sizeof(x))
+ 
+using ll = long long;
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, x, y;
+        cin >> n >> x >> y;
+        string a, b;
+        cin >> a >> b;
+        vector<int> v;
+        for (int i = 0; i < n; ++i) {
+            if (a[i] != b[i]) v.push_back(i);
+        }
+        int l = v.size();
+        if (l & 1) {
+            cout << -1 << endl;
+            continue;
+        }
+        if (l == 2) {
+            if (v[0] + 1 == v[1]) {
+                cout << min(x, 2 * y) << endl;
+            } else cout << y << endl;
+        } else
+            cout << 1ll* y * (l / 2)  << endl;
+    }
+ 
+}
